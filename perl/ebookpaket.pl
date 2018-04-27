516176af9ca3798255dc3c40982fea78441e4b00
Georg Mayr-Duffner <georg.mayr-duffner@wu.ac.at>
2018-04-27 15:45:31 +0200

    Add wrapper script

---
+++ perl/ebookpaket.pl
@@ -0,0 +1,106 @@
+############################################################
+#                                                          #
+# ebookpaket.pl - Aufbereitungsskript für EBook-Paket-     #
+#                 MARC-Dateien                             #
+#                                                          #
+# 2018-04-09 Georg Mayr-Duffner                            #
+#            georg.mayr-duffner@wu.ac.at                   #
+#                                                          #
+############################################################
+
+use Modern::Perl '2016';
+use Getopt::Long;
+use Catmandu;
+use File::Basename;
+use Log::Any::Adapter;
+use Log::Log4perl;
+use utf8;
+use open ':std', ':encoding(cp850)';
+
+GetOptions(#'verbose|v'       => \my $verbose,
+           'debug|d'         => \my $debug,
+           'input|i=s'       => \my $input,
+           'output|o=s'      => \my $output,
+           'sigel|s=s'       => \my $sigel,
+           'fixfile|fix|f=s' => \my $fixfile,
+           'help|hilfe|h|?'  => \my $help,
+           ) or usage();
+
+if ($help){usage()};
+
+my $dir = dirname(__FILE__);
+# $dir =~ s:\\:/:g;
+
+if ($debug){
+    Log::Any::Adapter->set('Log4perl');
+    Log::Log4perl::init("$dir/data/log4perl.conf");
+
+    my $logger = Log::Log4perl->get_logger('myprog');
+
+    $logger->info("Starting main program");
+}
+
+# Test for input file
+unless ($input){
+    say 'Bitte Eingabedatei angeben:';
+    chomp ($input = <STDIN>);
+}
+while (!-e $input){
+    say 'Eingabedatei existiert nicht. Bitte neu angeben:';
+    chomp ($input = <STDIN>);
+}
+
+unless ($output){
+    say "Bitte Namen für Ausgabedatei angeben und mit <Enter> bestätigen.\n",
+        'Wenn die Auswahl leergelassen wird, heißt die Ausgabedatei "output.mrc".';
+    chomp ($output = <STDIN>);
+    $output = 'output.mrc' unless $output;
+}
+
+unless ($sigel){
+    say 'Bitte ein Paketsigel angeben oder leer lassen und mit <Enter> bestätigen.';
+    chomp ($sigel = <STDIN>);
+    $sigel = 'leer' unless $sigel;
+}
+
+unless ($fixfile){
+    say 'Bitte ein Fixfile angeben oder leer lassen für Standardfixfile "ebook.fix".';
+    chomp ($fixfile = <STDIN>);
+    $fixfile = 'ebook.fix' unless $fixfile;
+}
+
+my $fixer = Catmandu::Fix->new(
+    variables => { ISO2MARC => $dir . '\data\iso3166H2marc.csv',
+                   MARC2ISO => $dir . '\data\marc2iso3166H.csv',
+                   sigel    => $sigel,
+                 },
+    fixes => [$fixfile],
+);
+
+my $importer = Catmandu->importer('MARC', file => $input);
+my $exporter = Catmandu->exporter('MARC', file => $output);
+my $fixed_importer = $fixer->fix($importer);
+
+$exporter->add_many($fixed_importer->benchmark);
+$exporter->commit;
+
+undef($exporter);
+
+sub usage {
+    print STDERR << "EOF";
+
+Dieses Programm dient zur Aufbereitung von Ebook-Metadaten.
+
+Verwendung:
+
+$0 --input <Dateipfad> [--output <Dateipfad> --sigel <sigel> --verbose --debug --help]
+
+    --input     -i          Pfad zur Eingabedatei
+    --output    -o          Pfad/Dateiname für die Ausgabedatei
+    --sigel     -s          Ein spezifisches Paketsigel (optional - wird, wenn
+                            möglich vom Fix-File eingetragen. Nötig bei Nomos!
+
+EOF
+    exit;
+}
+
