a8393b419f5dff2bc58a3ed7acfc6ad493e41b18
Georg Mayr-Duffner <georg.mayr-duffner@wu.ac.at>
2018-06-28 16:30:01 +0200

    Add check for fix file

--- perl/ebookpaket.pl
+++ perl/ebookpaket.pl
@@ -84,6 +84,7 @@ unless ($fixfile){
     say 'Bitte ein Fixfile angeben oder leer lassen für Standardfixfile "ebook.fix".';
     chomp ($fixfile = <STDIN>);
     $fixfile = 'ebook.fix' unless $fixfile;
+    $logger->error('No fix file') unless -e $fixfile;
 }
 
 my $fixer = Catmandu::Fix->new(
